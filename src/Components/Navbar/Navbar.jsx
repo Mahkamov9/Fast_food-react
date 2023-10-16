4eca1f3603f6560d42907d65ca75061fdf558545
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-16 23:17:46 +0500

    nav

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Navbar.css";
+
+
+export default function Navbar() {
+  return(
+      <>
+        <h1>Hello</h1>
+      </>
+  )
+}
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+
+export default function HomePage() {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
