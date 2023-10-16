35e7d7731b539d0bc9145f9a4efb7def35b8af60
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-17 02:44:37 +0500

    axios

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Navbar.css";
+import axios from 'axios';
 
 
 export default function Navbar() {
-  return(
+
+    useEffect(() =>{
+        axios.get('https://reqres.in/api/users?page=2')
+        .then(res => console.log(res))
+        // .catch(err => console.log(err));
+    }, [])
+
+
+    return(
       <>
-        <h1>Hello</h1>
+        <h2>Axios Library</h2>
+
       </>
   )
 }
