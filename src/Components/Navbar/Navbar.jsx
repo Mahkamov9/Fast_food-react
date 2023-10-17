a46bf67234ce6581bf846e1e8334bded1d3908da
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-17 23:34:56 +0500

    asdf

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,123 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Navbar.css";
 import axios from 'axios';
 
 
 export default function Navbar() {
+//   GET
 
-    useEffect(() =>{
+
+//     const [data, setData] = useState([])
+//     useEffect(() =>{
+//         axios.get('https://reqres.in/api/users?page=2')
+//         .then(res => setData(res.data.data))
+//         .catch(err => console.log(err));
+//     }, [])
+
+
+//     return(
+//       <>
+//         <h2>Axios Library</h2>
+//         {
+//             data.map((d, i) =>{
+//                 return <p key={i}>{d.last_name}</p>
+//             })
+//         }
+//         {
+//             data.map((d, i) =>{
+//                 return <h1 key={i}>{d.first_name}</h1>
+//             })
+//         }
+//       </>
+//   )
+
+
+
+
+    // POST
+
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     axios.post('https://reqres.in/api/users', {
+    //         "name": "Ahadjon",
+    //         "job": "Developer"
+    //     })
+    //         .then(res => console.log(res))
+    //         .catch(err => console.log(err));
+    // }, [])
+
+
+    // return (
+    //     <>
+    //         <h2>Axios Library</h2>
+    //         {
+    //             // data.map((d, i) => {
+    //             //     return <p key={i}>{d.last_name}</p>
+    //             // })
+    //         }
+
+    //     </>
+    // )
+
+
+
+
+
+
+
+
+    // UPDATE
+
+    const [data, setData] = useState([])
+    useEffect(() => {
         axios.get('https://reqres.in/api/users?page=2')
-        .then(res => console.log(res))
-        // .catch(err => console.log(err));
+            .then(res => setData(res.data.data))
+            .catch(err => console.log(err));
     }, [])
 
 
-    return(
-      <>
-        <h2>Axios Library</h2>
+    return (
+        <>
+            <h2>Axios Library</h2>
+            {
+                data.map((d, i) => {
+                    return <p key={i}>{d.first_name}</p>
 
-      </>
-  )
+                })
+            }
+
+            <h3>Axios</h3>
+            <h3>Axios</h3>
+            <h3>Axios</h3>
+            <h3>Axios</h3>
+            <h3>Axios</h3>
+            <h3>Axios</h3>
+            <h3>Axios</h3>
+
+
+        </>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
