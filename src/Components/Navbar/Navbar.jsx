c3b424ab5ad856e86a9f2c087b6c9835007a5608
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-18 23:16:59 +0500

    local s

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -85,33 +85,7 @@ export default function Navbar() {
 
                 })
             }
-
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
-            <h3>Axios</h3>
+            <h3>Local ctorage</h3>
 
 
         </>
