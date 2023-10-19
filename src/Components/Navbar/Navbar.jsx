43569b4cd472c155898331fbadd139fc78143411
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-19 23:23:27 +0500

    local saaaaaaaaaaaaaaaaaaaa

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -106,6 +106,7 @@ export default function Navbar() {
             <h3>Local ctorage</h3>
             <h3>Local ctorage</h3>
             <h3>Local ctorage</h3>
+            <h3>Local ctorage</h3>
 
 
         </>
