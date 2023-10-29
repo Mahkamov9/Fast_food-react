438ee59869e83e4ec7e8c60fcd474a78fffd2b23
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-29 19:20:36 +0500

    aside scale

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -20,8 +20,28 @@ export default function Aside() {
                 </div>
 
                 <div className="aside-left">
-                    <div className='aside-left-top'>1</div>
-                    <div className='aside-left-bottom'>2</div>
+                    <div className='aside-left-top'>
+                            <div className="aside-sale-box1 aside-sale-box2">
+                                <p className="aside-sale-box-num">only</p>
+                                <p className="aside-sale-box-text aside-sale-box-text2">17%</p>
+                            </div>
+                        <div className='aside-right-title-box aside-right-title-box2'>
+                            <h3 className='aside-right-subtitle'>Deal Of The Day</h3>
+                            <p className='aside-right-title aside-right-title2'>Quinoa & Black Bean Burger</p>
+                            <a className='aside-box-link' href="#">ORDER NOW</a>
+                        </div>
+                    </div>
+                    <div className='aside-left-bottom'>
+                            <div className="aside-sale-box1 aside-sale-box2 aside-sale-box3">
+                                <p className="aside-sale-box-num">only</p>
+                                <p className="aside-sale-box-text aside-sale-box-text2">17%</p>
+                            </div>
+                        <div className='aside-right-title-box aside-right-title-box2'>
+                            <h3 className='aside-right-subtitle'>Deal Of The Day</h3>
+                            <p className='aside-right-title aside-right-title2'>Quinoa & Black Bean Burger</p>
+                            <a className='aside-box-link' href="#">ORDER NOW</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
