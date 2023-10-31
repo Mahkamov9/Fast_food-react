efa1ee34a75af351c61874ebd55fddcf371e7cfc
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-10-31 23:52:50 +0500

    begin a

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,71 +4,7 @@
  export default function About() {
    return (
      <div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-     <div>About</div>
-
-
+        <h1>About</h1>
      </div>
    )
  }
