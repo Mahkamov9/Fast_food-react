afdde6eb2331d6d2049922c155462a41d1ccd150
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-11-01 23:08:08 +0500

    about item

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,22 +1,29 @@
- import React from 'react'
- import "./About.css";
+import React from 'react'
+import "./About.css";
 
- export default function About() {
-   return (
-     <>
+export default function About() {
+  return (
+    <>
       <div className="about">
         <div className="container">
           <div className="about-box">
-            <h3>Ingredients</h3>
-            <p>Baked with Natural Ingredients</p>
+            <div className='about-text-box'>
+              <h3>INGREDIENTS</h3>
+              <p>Baked with Natural <br /> Ingredients</p>
+            </div>
             <div className='about-list'>
-              <div className="about-item"></div>
-              <div className="about-item"></div>
+              <div className="about-item">
+                <p>Fresh Buns</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
+              </div>
+              <div className="about-item">
+                <img src="" alt="" />
+              </div>
               <div className="about-item"></div>
             </div>
           </div>
         </div>
       </div>
-     </>
-   )
- }
+    </>
+  )
+}
