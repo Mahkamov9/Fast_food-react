5b3407dffac300001a617e32e102a92c5828f7c5
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-11-01 17:22:42 +0500

    ssss

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,8 +3,15 @@
 
  export default function About() {
    return (
-     <div>
-        <h1>About</h1>
-     </div>
+     <>
+      <div className="about">
+        <div className="container">
+          <div className="about-box">
+            <h1>Salom</h1>
+            <h1>Salom</h1>
+          </div>
+        </div>
+      </div>
+     </>
    )
  }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import Aside from '../Components/Aside/Aside'
+import About from '../Components/About/About'
 
 export default function HomePage() {
   return (
     <>
         <Header/>
         <Aside/>
+        <About/>
     </>
   )
 }
