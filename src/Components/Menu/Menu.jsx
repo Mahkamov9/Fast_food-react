4c4436b07c04f745061802da180f1a14fa3d39a9
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-11-09 23:52:36 +0500

    fdsaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -84,6 +84,8 @@ export default function Menu() {
             <h1>Card Hold</h1>
             <h1>Card Hold</h1>
             <h1>Card Hold</h1>
+            <h1>Card Hold</h1>
+            <h1>Card Hold</h1>
           </div>
         </div>
       </div>
