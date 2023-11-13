063ab91b0340a5c4647798d90cdfa68d1388a8c6
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-11-13 23:38:20 +0500

    qweraaaaaaaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -91,6 +91,10 @@ export default function Menu() {
               <h1>Ssss</h1>
               <h1>Ssss</h1>
               <h1>Ssss</h1>
+              <h1>Ssss</h1>
+              <h1>Ssss</h1>
+              <h1>Ssss</h1>
+              <h1>Ssss</h1>
               {/* <img className="contact_img" src={CImg} alt="pic" /> */}
             </div>
           </div>
