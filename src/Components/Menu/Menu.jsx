bb64a27b167a9b2740ad7282ad92889acc46e5c2
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-11-15 22:35:35 +0500

    asfggqaaaaaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Menu.css";
-import { useState } from 'react';
+// import { useState } from 'react';
 
 export default function Menu() {
     return(
         <>
+            <h1>Salom</h1>
             <h1>Salom</h1>
             <h1>Salom</h1>
             <h1>Salom</h1>
