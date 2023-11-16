e3ec11da1c792059b2ce1a05d8a2d291720681e8
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-11-16 23:35:04 +0500

    asfggqaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -43,7 +43,8 @@ export default function Menu() {
             <h1>Salom</h1>
             <h1>Salom</h1>
             <h1>Salom</h1>
-            <h1>Salom</h1>      ``
+            <h1>Salom</h1>
+            <h1>Salom</h1>      
         </>
     )
 }
