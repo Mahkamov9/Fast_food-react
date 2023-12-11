fecdc49397946ce1d7f6d873d941f38fbf67c1e8
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-12-11 21:07:52 +0500

    sasf23re4aaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -142,6 +142,10 @@ export default function Menu() {
             <h1>Thank You</h1>
             <h1>Thank You</h1>
             <h1>Thank You</h1>
+            <h1>Thank You</h1>
+            <h1>Thank You</h1>
+            <h1>Thank You</h1>
+            <h1>Thank You</h1>
         </>
 
 
