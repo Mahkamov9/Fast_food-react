f82c3b0a2e6ffc4434903f7fc695b90629191c3e
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2024-01-07 23:50:04 +0500

    20241aaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -12,6 +12,8 @@ export default function Menu() {
             <h1>New Year</h1>
             <h1>New Year</h1>
             <h1>New Year</h1>
+            <h1>New Year</h1>
+            <h1>New Year</h1>
 
         </>
 
