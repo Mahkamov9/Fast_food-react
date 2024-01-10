2f32b6cd29d7b26464131377caa6142cad2476ee
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2024-01-10 23:35:44 +0500

    stbranch aaaaaaaaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -68,6 +68,9 @@ export default function Menu() {
             <h1>New Year</h1>
             <h1>New Year</h1>
             <h1>New Year</h1>
+            <h1>New Year</h1>
+            <h1>New Year</h1>
+            <h1>New Year</h1>
 
         </>
 
