f7a24fc345ffeafb4527911d11146d9ddcdf4462
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2024-01-11 23:58:46 +0500

    stbranch aaaaaaaaaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,77 +1,21 @@
 import React from 'react'
 import "./Menu.css";
 import ContactCode from './Contact_Code';
+import MenuCard from '../MenuCard/MenuCard';
 // import { useState } from 'react';
 
 export default function Menu() {
     return (
         <>
-            <ContactCode/>
-
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-            <h1>New Year</h1>
-
+            <div className="menu">
+                <div className="container">
+                    <h1 className='menu_title'>OUR MENU</h1>
+                    <p className="menu_subtitle">Choose & Enjoy</p>
+                    <div>
+                        <MenuCard/>
+                    </div>
+                </div>
+            </div>
         </>
 
 
---
+++ src/Components/MenuCard/MenuCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './MenuCard.css';
+
+import CardBurger from '../../Assets/Img/burger-06-free-img.png';
+
+export default function MenuCard() {
+    return (
+        <>
+            <div className='menu_card'>
+                <img src={CardBurger} alt="Rasm" />
+                <div className='menu_card_box'>
+                    <p>Chiken Burger</p>
+                    <p>$4.99</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod.</p>
+                    <button>➕</button>
+                </div>
+            </div>
+        </>
+    )
+}
