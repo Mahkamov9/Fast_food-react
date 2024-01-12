aecae37b378e90ad47fe47a8fed75f136652d1d9
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2024-01-12 23:36:29 +0500

    stbranch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -15,6 +15,7 @@ export default function Menu() {
                         <MenuCard/>
                         <MenuCard/>
                         <MenuCard/>
+                        <MenuCard/>
                     </div>
                 </div>
             </div>
