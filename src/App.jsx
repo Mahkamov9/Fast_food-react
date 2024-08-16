ba5a8d6cd5384413bc762a7cae613084b9e7f410
Ahadjon Mahkamov <94121122+Mahkamov9@users.noreply.github.com>
2024-08-16 22:33:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-тсосослалаорттлслаfhjfjfdjfhhdhnxnimportbcfjjf './App.css';
+алтсосослалаорттлслаfhjfjfdjfhhdhnxnimportbcfjjf './App.css';
 import HomePage from './Pages/HomePage';
 
 function App() {
